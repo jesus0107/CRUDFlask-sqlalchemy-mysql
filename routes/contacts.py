80e0b52d059f8420a5bd8ea15d2e8b30983883f1
Jesus <bep010795@gmail.com>
2022-09-04 16:54:40 -0500

    Eliminando contacto

--- routes/contacts.py
+++ routes/contacts.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect,url_for
 from utils.db import db
 from models.contact import Contact
 
@@ -6,7 +6,8 @@ contacts = Blueprint('contacts', __name__)
 
 @contacts.route('/')
 def index():
-    return render_template('index.html')
+    contacts = Contact.query.all()
+    return render_template('index.html', contacts = contacts)
 
 @contacts.route('/new', methods=['POST'])
 def add_contact():
@@ -17,15 +18,18 @@ def add_contact():
 
     db.session.add(new_contact)
     db.session.commit()
-    return redirect('/')
+    return redirect(url_for('contacts.index'))
 
 @contacts.route('/update')
 def update_contact():
     return "Updateing contact"
     
-@contacts.route('/delete')
-def delete_contact():
-    return "deleted contact"
+@contacts.route('/delete/<id>')
+def delete_contact(id):
+    contact_id = Contact.query.get(id)
+    db.session.delete(contact_id)
+    db.session.commit()
+    return redirect(url_for('contacts.index'))
 
 @contacts.route('/about')
 def about():
