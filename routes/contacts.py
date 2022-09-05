c244254dadd95de4a638c7265250cfcbdf9566f2
Jesus <bep010795@gmail.com>
2022-09-04 22:32:51 -0500

    Editando contacto

--- routes/contacts.py
+++ routes/contacts.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Blueprint, render_template, request, redirect,url_for
 from utils.db import db
 from models.contact import Contact
@@ -20,14 +21,22 @@ def add_contact():
     db.session.commit()
     return redirect(url_for('contacts.index'))
 
-@contacts.route('/update')
-def update_contact():
-    return "Updateing contact"
+@contacts.route('/update/<id>', methods=['POST', 'GET'])
+def update_contact(id):
+    contact = Contact.query.get(id)
+    if request.method == 'POST':
+        contact.full_name = request.form['full_name']
+        contact.email = request.form['email']
+        contact.phone = request.form['phone']
+        db.session.commit()
+        return redirect(url_for('contacts.index'))
+    
+    return render_template('update.html', contact=contact)
     
 @contacts.route('/delete/<id>')
 def delete_contact(id):
-    contact_id = Contact.query.get(id)
-    db.session.delete(contact_id)
+    contact = Contact.query.get(id)
+    db.session.delete(contact)
     db.session.commit()
     return redirect(url_for('contacts.index'))
 
